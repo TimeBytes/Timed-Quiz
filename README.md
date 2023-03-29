b21982a6dbe7048f416f25ae49636e9f73c279ce
TimeBytes <123673532+TimeBytes@users.noreply.github.com>
2023-03-29 13:34:14 -0400

    added comments to html,javascript and css and added content to readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Web-Dev-Quiz
+# Timed-Quiz
+
+## Description
+
+This project uses manipulation of the DOM to dymanically display information. Being able to comfortably change the html, access information from the html and attach on click events to elements in html was recently taught in the bootcamp im enrolled in. This project taught me how to traverse the DOM, create new elements in javascript, append those elements, set timed intervals, add on click events, change the data state of an element, dynamically display information on screen and how to manage data stored in local storage.
+
+## Usage
+
+When the webpage is first loaded, there is the instructions of the quiz, timer will decrease if questions are answered incorrectly, and quiz will end if timer runs out or all answers are selected.
+![screenshot of landing page](./assets/screenshot-landing-screen.png)
+
+The quiz questions will be displayed as followed, after clicking on an answer, there will be a message displayed below stating if the selected answer was correct or wrong, message will display for 1 second.
+
+![screenshot of quiz questions](./assets/screenshot-quiz-questions.png)
+
+You will then be shown the screen where you can enter you initals so that your initals and score will be saved in the local storage.
+![screenshot of enter-initals page](./assets/screenshot-enter-initals.png)
+
+You can access the highscore screen through the header "Your Highscores" link [first image], or after submitting your initals [second image].
+![screenshot of the link to press to get to highscores](./assets/screenshot-check-highscore.png)
+![screenshot of the submit button](./assets/screenshot-enter-initals-submit-button.png)
+
+This is what the highscores page looks like
+![screenshot of highscore page](./assets/screenshot-highscores.png)
+
+On the highscore screen you will find 2 buttons, one to go back to the beginning of the quiz [first image] and the other is to clear your highscores [second image].
+![screenshot of with circle around go back button](./assets/screenshot-highscores-go-back-button.png)
+![screenshot of with circle around clear highscores button](./assets/screenshot-highscores-clear-highscores.png)
+
+This is what the page would look like after pressing the Clear Highscores button
+![screenshot of highscore page after pressing the clear highscores button](./assets/screenshot-highscores-cleared.png)
+
+Live Deployed URL: https://timebytes.github.io/Timed-Quiz/
+
+## Credits
+
+This webpage was designed by a mock up from the University of Toronto Bootcamp
+https://courses.bootcampspot.com/courses/3937/assignments/50865?module_item_id=920642
+
+## License
+
+MIT License
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// list of objects containing the question as a string, options as a list and answer as a string
 const listOfQuestions = [
   {
     question: "Commonly used data types DO NOT include:",
@@ -33,68 +34,97 @@ const listOfQuestions = [
     answer: "Console.log",
   },
 ];
+
 var score = 0;
 var userAnswer = [];
 var userHighscore = [];
 var currentQuestion = 0;
 var startTime = 60;
-var countDown = document.querySelector("#timer");
-var start = document.querySelector("#start");
+var startInterval;
+var headerEl = document.querySelector("#header-container");
+var counDownEl = document.querySelector("#timer");
+var startEl = document.querySelector("#start");
+var highscoresEl = document.querySelector("#highscores");
+var startQuizEl = document.querySelector("#start-quiz");
+var highscoreListEl = document.querySelector("#highscore-list");
+var checkHighscoresEl = document.querySelector("#check-highscores");
+var submitButton = document.querySelector("#submit");
+var enterInitialsEl = document.querySelector("#enter-initals");
+var isAnswerCorrectEl = document.querySelector("#is-answer > p");
+var isAnswerCorrectContainer = document.querySelector("#is-answer");
+var goBackButton = document.querySelector("#go-back");
+var clearHighscores = document.querySelector("#clear-highscores");
+var quizSection = document.querySelector("#quiz-section");
 var questionH2 = document.createElement("h2");
-var section = document.querySelector("#start-quiz");
 var quizQuestion = document.createElement("h2");
 var orderedList = document.createElement("ol");
-var highscores = document.querySelector("#highscores");
-var highscoreList = document.querySelector("#highscore-list");
-var checkHighscores = document.querySelector("#check-highscores");
-var submit = document.querySelector("#submit");
-var enterInitials = document.querySelector("#enter-initals");
 
+//for the list length of listOfQuestions loop and display the questions with the options on screen by creating elemtns and appending them
 function displayQuestion() {
-  section.dataset.state = "hidden";
-  var quizSection = document.querySelector("#quiz-section");
+  startQuizEl.dataset.state = "hidden";
+  quizSection.dataset.state = "visible";
+  quizSection.setAttribute("data-state", "visible");
   orderedList.textContent = "";
+  //validates which question to display and that it doesn't go past the length of the list
   if (currentQuestion < listOfQuestions.length) {
     quizQuestion.textContent = listOfQuestions[currentQuestion].question;
     quizSection.appendChild(quizQuestion);
     quizSection.appendChild(orderedList);
+    //loops the options available and displays them on screen by creating list element and changing it's content then appending it
     for (let i = 0; i < listOfQuestions[currentQuestion].options.length; i++) {
       var quizOptions = document.createElement("li");
       quizOptions.setAttribute("class", "quiz-options");
       quizOptions.textContent = listOfQuestions[currentQuestion].options[i];
       orderedList.appendChild(quizOptions);
     }
+    //adds on click event to the list items as they were dynamically created, loops through each option
     var selectAnswer = document.querySelectorAll(".quiz-options");
     selectAnswer.forEach(createClickEvent);
+    //on click event, will check if the slected option was correct or wrong and increment score if correct, minus the timer if wrong
     function createClickEvent(eachSelectAnswer) {
       eachSelectAnswer.addEventListener("click", function (event) {
         if (
           listOfQuestions[currentQuestion].answer == event.target.textContent
         ) {
+          isAnswerCorrectEl.textContent = "Correct!";
           score++;
         } else {
           startTime -= 10;
+          isAnswerCorrectEl.textContent = "Wrong!";
         }
+        displayIsAnswerCorrect();
+        //increments the question to be displayed
         currentQuestion++;
+        //recursive function call
         displayQuestion();
       });
     }
   } else {
     quizSection.textContent = "";
-    console.log("why here");
     displayEnterName();
   }
 }
 
+//adds a delay to the display answer, will show up for 1 second after selecting an option.
+function displayIsAnswerCorrect() {
+  isAnswerCorrectContainer.dataset.state = "visible";
+  setTimeout(function () {
+    isAnswerCorrectContainer.dataset.state = "hidden";
+  }, 1000);
+}
+
+//displays finaly score on screen and a text input for user's initals
 function displayEnterName() {
   clearInterval(startInterval);
   document.querySelector("input").value = "";
-
-  enterInitials.dataset.state = "visible";
+  enterInitialsEl.dataset.state = "visible";
   var finalScore = document.querySelector(".final-score");
+  //converts the score into a % grade
   score = (score / listOfQuestions.length) * 100;
   finalScore.textContent = "Your final score is: " + score + "%";
 }
+
+//updates the userHighscore variable with localstorage value, if none, sets the list to empty
 function checkLocalStorage() {
   if (JSON.parse(localStorage.getItem("highscores")) == null) {
     userHighscore = [];
@@ -102,10 +132,12 @@ function checkLocalStorage() {
     userHighscore = JSON.parse(localStorage.getItem("highscores"));
   }
 }
+
+//starts the countdown timer and when timer reaches 0 quiz is over and it calls displayEnterName function
 function timer() {
   startInterval = setInterval(function () {
     startTime--;
-    countDown.textContent = "Time: " + startTime;
+    counDownEl.textContent = "Time: " + startTime + " second(s)";
     if (startTime <= 0) {
       var quizSection = document.querySelector("#quiz-section");
       quizSection.textContent = "";
@@ -114,59 +146,69 @@ function timer() {
   }, 1000);
 }
 
+// displays the highscore screen and hides the header
 function displayHighscores() {
-  highscoreList.textContent = "";
+  highscoreListEl.textContent = "";
+  headerEl.dataset.state = "hidden";
   for (let i = 0; i < userHighscore.length; i++) {
-    console.log(userHighscore);
     var highscoresListElement = document.createElement("li");
     highscoresListElement.setAttribute("class", "user-highscore-list-item");
     highscoresListElement.textContent = userHighscore[i];
-    highscoreList.appendChild(highscoresListElement);
+    highscoreListEl.appendChild(highscoresListElement);
   }
 }
 
-start.addEventListener("click", function () {
+//add on click event listener to the start quiz button
+startEl.addEventListener("click", function () {
   displayQuestion();
   timer();
 });
 
-checkHighscores.addEventListener("click", function () {
-  section.dataset.state = "hidden";
-  highscores.dataset.state = "visible";
-  enterInitials.dataset.state = "hidden";
+//add on click event listener to the "your highscore" link in the header, hides sections that are not needed and calls the displayHighscores function
+checkHighscoresEl.addEventListener("click", function () {
+  //stops the timer incase user clicks "Your highscore" when the quiz has started.
+  clearInterval(startInterval);
+  startQuizEl.dataset.state = "hidden";
+  highscoresEl.dataset.state = "visible";
+  enterInitialsEl.dataset.state = "hidden";
+  quizSection.dataset.state = "hidden";
   checkLocalStorage();
   displayHighscores();
 });
 
-var goBackButton = document.querySelector("#go-back");
-var clearHighscores = document.querySelector("#clear-highscores");
-
+//add on click event to the go back button to restart the quiz
 goBackButton.addEventListener("click", function () {
   currentQuestion = 0;
   score = 0;
   startTime = 60;
-  countDown.textContent = "Time: " + startTime;
-  section.dataset.state = "visible";
-  highscores.dataset.state = "hidden";
+  counDownEl.textContent = "Time: " + startTime;
+  startQuizEl.dataset.state = "visible";
+  highscoresEl.dataset.state = "hidden";
+  headerEl.dataset.state = "visible";
 });
 
+//add on click event to clear high score button and removes the localstorage key "highscores" and clears the list of highscores on screen
 clearHighscores.addEventListener("click", function () {
   localStorage.removeItem("highscores");
-  highscoreList.textContent = "";
+  highscoreListEl.textContent = "";
 });
 
-submit.addEventListener("click", function (event) {
+//add on click event to the submit button, prevent default from happening,
+//stores the input value into a list and get converted into a string to store in localstorage then calls displayhighscores function
+submitButton.addEventListener("click", function (event) {
   event.preventDefault();
   var textInput = document.querySelector("input").value;
+  //capitalize the initals
+  textInput = textInput.toUpperCase();
+  //validation to ensure user input is not empty
   if (textInput == "") {
     alert("Please enter your initals");
   } else {
     checkLocalStorage();
     userHighscore.push(textInput + " - " + score + "%");
     localStorage.setItem("highscores", JSON.stringify(userHighscore));
-    enterInitials.dataset.state = "hidden";
-    highscores.dataset.state = "visible";
-    console.log("world");
+    enterInitialsEl.dataset.state = "hidden";
+    highscoresEl.dataset.state = "visible";
     displayHighscores();
   }
 });
